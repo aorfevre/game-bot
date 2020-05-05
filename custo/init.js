884c0f23f3bcfb4ac1c9f89fd6c19da1d0f5a137
Alexandre <aorfevre@gmail.com>
2020-05-05 22:56:49 +0200

    update

---
+++ custo/init.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const TelegramBot = require('node-telegram-bot-api');
+var async = require('async');
+
+
+module.exports.setTelegram = function() {
+
+  var telegramLive = '1000558929:AAG7aAPgu9WNDjTUGRxQHQ0QG2TrP7d27Hw';
+  var telegramTest = '602801351:AAEAUmijk5htvA6_W_14cbdkkVjFuXYzT_g';
+
+  var telegramToken = telegramLive
+  global.isDev = __dirname.indexOf("ablock") !== -1
+
+  return new TelegramBot(telegramToken, {
+    polling: {
+      interval: 200,
+      limit: 75,
+      autoStart: true,
+      allowed_updates: ["message", "inline_query", "callback_query"]
+    }
+  });
+
+}
