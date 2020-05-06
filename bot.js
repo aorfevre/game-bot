6b93a2f4fc53c2697c387a322a8227eddcd7f0c8
Alexandre <aorfevre@gmail.com>
2020-05-06 23:25:09 +0200

    update

--- bot.js
+++ bot.js
@@ -6,7 +6,9 @@ global.bot = init.setTelegram();
 console.log("start")
 
 bot.on('message', function(event) {
-  if (event.text !== undefined && event.text.toLowerCase().indexOf("@team") !== -1 && event.chat.id === -1001162960241) {
+  console.log("event message", event)
+  if (event.text !== undefined &&
+    event.text.toLowerCase().indexOf("@team") !== -1 && event.chat.id === -1001162960241) {
     var _txt = "☝️☝️☝️☝️☝️☝️☝️\n" + "@Simosss @solutionniste @aorfevrebr @blokcove"
     bot.sendMessage(event.chat.id, _txt)
   }
