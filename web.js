9262c55e5f9329afbd29070bb3fed8881e54889c
Alexandre <aorfevre@gmail.com>
2020-05-09 19:57:46 +0200

    update

--- web.js
+++ web.js
@@ -9,13 +9,22 @@ var port = process.env.PORT || 8708;
 app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
-// set the home page route
-app.get('/', function(req, res) {
+// parse the updates to JSON
+app.use(express.json());
 
-  // ejs render automatically looks in the views folder
-  res.render('index');
+// // set the home page route
+// app.get('/', function(req, res) {
+//
+//   // ejs render automatically looks in the views folder
+//   res.render('index');
+// });
+
+// We are receiving updates at the route below!
+app.post(`/bot${TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
 });
 
 app.listen(port, function() {
