2d5818e9742f31d5a9b492869aa2c251858e74ff
aorfevre <aorfevre@gmail.com>
2020-06-25 23:38:52 +0200

    update

--- admin/admin_board.js
+++ admin/admin_board.js
@@ -152,7 +152,7 @@ module.exports.getGroupsName = function(msg, myUser) {
         });
         for (var i in results) {
           if (results[i].title !== undefined)
-            _txt += "🔹" + results[i].title + " - " + results[i].count + "\n "
+            _txt += "🔹" + results[i].title + " - " + results[i].count + "\n"
           // console.log(results[i].title, "/", results[i].count)
         }
 
