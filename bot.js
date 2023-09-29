faa249d035fcbb273b04afa058dfeca186165c49
alex <aorfevre@gmail.com>
2023-09-29 14:37:05 +0200

    upd

--- bot.js
+++ bot.js
@@ -6,7 +6,29 @@ global.bot = init.setTelegram();
 //
 bot.on('message', function(event) {
  
-    console.log('Event', event)
-      bot.sendMessage(event.chat.id, "Pong")
   
 })
+
+bot.onText(/^\/[p]ay(.+|\b)/, (msg, match) => {
+  console.log('msg',msg)
+  var _markup = [];
+  _markup.push([
+    {
+      text: "Pay 5 Rock",
+      url: "https://game-payment-urphktcmtq-ew.a.run.app?price=5&mode=rock",
+    },
+    
+  ]);
+
+  
+
+  var options = {
+    parse_mode: "HTML",
+    disable_web_page_preview: true,
+    reply_markup: JSON.stringify({
+      inline_keyboard: _markup,
+    }),
+          };
+  bot.sendMessage(msg.chat.id, "Let's create a payment link",options)
+
+});
