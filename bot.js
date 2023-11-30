c92d9a79f8495610c04d211064b2f32253a60ba9
alex <aorfevre@gmail.com>
2023-11-30 13:34:23 +0100

    Remove logs

--- bot.js
+++ bot.js
@@ -42,16 +42,13 @@ bot.on("message", async (msg) => {
         await helper.checkReferralSystem(msg);
       }
     } else {
-      console.log('Starting... ')
 
       try {
 
         const user = await helper.updateUser(msg);
-      console.log('User updated... ')
 
         if (user.isReferred) {
           await helper.home(msg);
-      console.log('Home presented... ')
 
         } else {
           await helper.referralSystem(msg);
